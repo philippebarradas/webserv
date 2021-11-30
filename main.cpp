65adaf5295b076b7d09e674bf720909bd62ec88e
dodjian <dovdjianpro@gmail.com>
2021-11-30 12:58:55 +0100

    + try to add accept

--- main.cpp
+++ main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tsannie <tsannie@student.42.fr>            +#+  +:+       +#+        */
+/*   By: dodjian <dovdjianpro@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/30 10:09:47 by tsannie           #+#    #+#             */
-/*   Updated: 2021/11/30 12:18:56 by tsannie          ###   ########.fr       */
+/*   Updated: 2021/11/30 12:58:15 by dodjian          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,18 +21,22 @@
 
 int	main( void )
 {
-	int sockfd;
+    int fd, sockfd;
+    socklen_t addrlen;
 	struct sockaddr_in sin;
+	struct sockaddr_in client;
 
-	//sin.sin_addr.s_addr = INADDR_ANY;
+	sin.sin_addr.s_addr = INADDR_ANY;
 	sin.sin_family      = AF_INET;
 	sin.sin_port        = htons(2000);
 
-    inet_aton("63.161.169.137", &sin.sin_addr.s_addr);
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	bind(sockfd, (struct sockaddr *)sin, sizeof(sin));
-
-	/* ... */
-
+    sockfd = socket(AF_INET, SOCK_STREAM, 0); // crée un point de communication, et renvoie un fd
+	bind(sockfd, reinterpret_cast<struct sockaddr *>(&sin), sizeof(sin)); // bind() affecte l'adresse spécifiée dans addr à la socket référencée par le descripteur de fichier sockfd
+    listen(sockfd, 1); // marque la socket comme passive, socket qui sera utilisé pour accepter les connexions
+    memset(&client, 0, sizeof(client));
+    addrlen = sizeof(client);
+    std::cout << "fd: " << fd << std::endl;
+    fd = accept(sockfd,(struct sockaddr*)&client, &addrlen); // crée une nouvelle socket connecté grâce au sockfd au premier dans la file d'attente de sockfd à l'écoute 
+    std::cout << "fd: " << fd << std::endl;
 	return 0;
 }
