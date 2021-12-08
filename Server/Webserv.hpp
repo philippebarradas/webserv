eb310dfc87c797b3d346adbc2e0a299146a05ca1
dovi06 <dovdjianpro@gmail.com>
2021-12-08 18:19:36 +0100

    [fix] try to do epoll again, check close_conn

--- Server/Webserv.hpp
+++ Server/Webserv.hpp
@@ -2,8 +2,10 @@
 # define WEBSERV_HPP
 
 // C++
-# include <iostream>
-# include <string>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cstring>
 
 // C
 #include <stdio.h>
@@ -16,6 +18,7 @@
 // Socket
 #include <sys/ioctl.h>
 #include <sys/poll.h>
+#include <sys/epoll.h>
 #include <sys/socket.h>
 #include <sys/time.h>
 #include <arpa/inet.h>
@@ -28,12 +31,23 @@ class Webserv
 		Webserv();
 		Webserv( Webserv const & src );
 		~Webserv();
-
+		int		my_bind(int listen_sd, struct sockaddr_in addr); // int ret in arg maybe
+		void	loop_life_server(struct epoll_event events[200]);
+		void	loop_epoll_fd_is_listening(struct epoll_event events[200], int end_server);
+		void	loop_connection(struct epoll_event events[200], int close_conn, int i);
+		int		my_read(struct epoll_event events[200], int close_conn, int i);
+		int		my_send(struct epoll_event events[200], int close_conn, int i,
+				int nbr_bytes_read);
+		//int	add_port(); // later
 		Webserv &		operator=( Webserv const & rhs );
 
 	private:
 		int	fd_socket;
-
+		int	fd_listen;
+		int	port;
+		int timeout; // time before epoll expiration
+		int nfds; // nbr fd
+		char buffer[10000];
 };
 
 std::ostream &			operator<<( std::ostream & o, Webserv const & i );
