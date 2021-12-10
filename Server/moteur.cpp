bd57468ff1a058f4362965dfa8c6109cfb543480
dodjian <dovdjianpro@gmail.com>
2021-12-10 14:04:14 +0100

    [+] moteur qui marche requete par requete

--- Server/moteur.cpp
+++ Server/moteur.cpp
@@ -6,7 +6,7 @@
 /*   By: dodjian <dovdjianpro@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/09 16:27:13 by dodjian           #+#    #+#             */
-/*   Updated: 2021/12/09 22:04:00 by dodjian          ###   ########.fr       */
+/*   Updated: 2021/12/10 14:02:45 by dodjian          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,6 @@
 #include <iostream>
 #include <fstream>
 #define PORT 8080
-
-void	display_all_fd_events(int i_conn, struct pollfd fds[200])
-{
-	std::cout << "i_conn = " << i_conn << std::endl;
-	for (int i = 0; i < i_conn && i < 200 && fds[i].fd > 0; i++)
-	{
-		std::cout << "fds[" << i << "] = " << fds[i].fd << std::endl;
-	}
-}
 
 void	loop_accept(int listen_fd, int new_socket, int i_conn, struct pollfd fds[200])
 {
@@ -50,20 +41,20 @@ void	loop_accept(int listen_fd, int new_socket, int i_conn, struct pollfd fds[20
 void	loop_server(int listen_fd, int new_socket, struct pollfd fds[200])
 {
 	int valread = 0;
-	int i_conn = 0;
+	int i_conn;
 	int nbr_connexions = 0;
 	char buffer[10000];
 
 	bzero(&buffer, sizeof(buffer));
 	while (1)
 	{
-		display_all_fd_events(i_conn, fds);
-		poll(fds, 200, 1); // 100 = delai en ms tant qu'il se passe rien
-		//if (fds[i_conn].fd == listen_fd)
-		if (fds[i_conn].events == POLLIN)
+		for (int i = 0; fds[i].fd > 0; i++)
+			std::cout << "fds[" << i << "] = " << fds[i].fd << std::endl;
+		poll(fds, 200, -1); // 100 = delai en ms tant qu'il se passe rien
+		//std::cout << "i_conn = " << i_conn << std::endl;
+		if (fds[i_conn].fd == listen_fd)
+		//if (fds[i_conn].events == POLLIN)
 		{
-			if (i_conn == 0)
-				i_conn++;
 			//loop_accept(listen_fd, new_socket, i_conn, fds);
 			//for (nbr_connexions = i_conn + 1; nbr_connexions < i_conn + 3; nbr_connexions++) // 3 est fixe
 			//{
@@ -75,15 +66,14 @@ void	loop_server(int listen_fd, int new_socket, struct pollfd fds[200])
 				std::cout << "new_socket = fefe" << new_socket << std::endl;
 				break ;
 			}
-			fds[i_conn].fd = new_socket;
-			fds[i_conn].events = POLLIN;
-			if (i_conn != 1)
-				i_conn++;
+			fds[nbr_connexions + 1].fd = new_socket;
+			fds[nbr_connexions + 1].events = POLLIN;
+			nbr_connexions++;
 			//}
 		}
 		else
 		{
-			std::cout << "i_conn = " << i_conn << std::endl;
+			std::cout << "else i_conn = " << i_conn << std::endl;
 			//valread = read( new_socket , buffer, 1024);
 			valread = recv(fds[i_conn].fd, buffer, sizeof(buffer), 0);
 			if (valread == -1)
@@ -108,6 +98,7 @@ void	loop_server(int listen_fd, int new_socket, struct pollfd fds[200])
 			}
 			ifs.close();
 			//std::cout << file << std::endl;
+			fds[i_conn].events = POLLOUT;
 			int nbr_bytes_send = 0;
 			nbr_bytes_send = send(fds[i_conn].fd, file.c_str(), file.size(), 0);
 			if (nbr_bytes_send == -1)
@@ -116,11 +107,17 @@ void	loop_server(int listen_fd, int new_socket, struct pollfd fds[200])
 				break ;
 			}
 			std::cout << "Hello message sent" << std::endl;
-			std::cout << "i_conn = " << i_conn << std::endl;
+
+			close(fds[i_conn].fd);
+			int j = 0;
+			for (j = 0; j < i_conn; j++){}
+			for (j = i_conn; fds[j + 1].fd > 0 && j < nbr_connexions; j++){}
+				fds[j].fd = fds[j + 1].fd;
+			nbr_connexions--;
+			i_conn = -1;
 		}
 		std::cout << "nbr_connexions = " << nbr_connexions << std::endl;
-		//i_conn += nbr_connexions;
-		//i_conn++;
+		i_conn++;
 	}
 	//send(new_socket , hello , strlen(hello) , 0 );
 }
