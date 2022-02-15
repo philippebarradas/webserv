d8856a35facf198a3bf5c7a560b93d46368edfc1
dodjian <dovdjianpro@gmail.com>
2022-02-15 16:32:28 +0100

    [fix] upload.php change

--- www/upload.php
+++ www/upload.php
@@ -1,18 +1,34 @@
-<?php
-// In PHP versions earlier than 4.1.0, $HTTP_POST_FILES should be used instead
-// of $_FILES.
+<!DOCTYPE html>
+<html>
+<body>
+    <?php
+    // In PHP versions earlier than 4.1.0, $HTTP_POST_FILES should be used instead
+    // of $_FILES.
 
-$uploaddir = '/home/user42/Bureau/webserv/www/uploads/';
-$uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
+    $uploaddir = '/home/user42/Bureau/webserv/www/uploads';
+    $uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
 
-echo '<pre>';
-if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
-    echo "File is valid, and was successfully uploaded.\n";
-} else {
-    echo "Possible file upload attack!\n";
-}
 
-echo 'Here is some more debugging info:';
-print_r($_FILES);
+    echo '<pre>';
+    if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
+        echo "File is valid, and was successfully uploaded.\n";
+    } else {
+        echo "Possible file upload attack!\n";
+    }
 
-print "</pre>";
+    echo 'Here is some more debugging info:';
+    print_r($_FILES);
+
+    print "</pre>";
+    ?>
+<form action="upload.php" method="post" enctype="multipart/form-data">
+	Select image to upload:
+	<input type="file" name="userfile" id="userfile">
+	<input type="submit" value="Upload Image" name="submit">
+</form>
+
+<img id="userfile"> </img>
+
+</body>
+</html>
+
