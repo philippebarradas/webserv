a1da62f998cb2027b9c27bc51c63d8a44200df06
tsannie <tsannie@student.42.fr>
2022-02-22 14:08:27 +0100

    [fix] cgi

--- srcs/Cgi/Cgi.hpp
+++ srcs/Cgi/Cgi.hpp
@@ -6,7 +6,7 @@
 /*   By: tsannie <tsannie@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/14 11:17:44 by dodjian           #+#    #+#             */
-/*   Updated: 2022/02/16 20:01:05 by tsannie          ###   ########.fr       */
+/*   Updated: 2022/02/22 13:41:12 by tsannie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,6 +72,7 @@ class Cgi
 
 		// METHODS
 		void	delete_argv_env(char **argv, char **env);
+		std::string	normVar( std::string src );
 		std::string	to_string(size_t nb);
 		bool	is_file_cgi(std::string path_extension);
 		void	init_path(std::string const & root, std::string const & path, std::string const & pathCgi);
